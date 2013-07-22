7c1e52b0e07902debc1ddeeb1078d140cf9bf15f
nandacms@gmail.com <nandacms@gmail.com@5cbca821-5f6d-a01b-806f-ce45c106f15d>
2013-07-22 18:00:24 +0000

    Definição dos parâmetros da classe Refeicao e métodos de encapsulamento da mesma.

--- EasyTicket/src/negocio/Refeicao.java
+++ EasyTicket/src/negocio/Refeicao.java
@@ -9,5 +9,56 @@ package negocio;
  * @author Nanda
  */
 public class Refeicao {
+    private int idRefeicao;
+    private EnumRefeicao descricao;
+    private float valor;
+    
+    public Refeicao(float valor){
+        this.valor = valor;        
+    }
+
+    /**
+     * @return the idRefeicao
+     */
+    public int getIdRefeicao() {
+        return idRefeicao;
+    }
+
+    /**
+     * @param idRefeicao the idRefeicao to set
+     */
+    public void setIdRefeicao(int idRefeicao) {
+        this.idRefeicao = idRefeicao;
+    }
+
+    /**
+     * @return the descricao
+     */
+    public EnumRefeicao getDescricao() {
+        return descricao;
+    }
+
+    /**
+     * @param descricao the descricao to set
+     */
+    public void setDescricao(EnumRefeicao descricao) {
+        this.descricao = descricao;
+    }
+
+    /**
+     * @return the valor
+     */
+    public float getValor() {
+        return valor;
+    }
+
+    /**
+     * @param valor the valor to set
+     */
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+    
+    
     
 }
