e4107ac16407a996515953bec4eeb7586d2825d5
marcela.domingues@gmail.com <marcela.domingues@gmail.com@5cbca821-5f6d-a01b-806f-ce45c106f15d>
2013-07-22 20:15:50 +0000

    Criação das classes Aluno, Funcionario e Pessoa.

--- trunk/EasyTicket/src/negocio/Aluno.java
+++ trunk/EasyTicket/src/negocio/Aluno.java
@@ -4,10 +4,62 @@
  */
 package negocio;
 
+import java.util.Date;
+
 /**
  *
  * @author Nanda
  */
-public class Aluno {
+public class Aluno extends Pessoa {
+    
+    private Curso curso;
+    private EnumStatusAluno statusAluno;
+    private String inicioCursoAluno;
+    private String terminoCursoAluno;
+    
+    public Aluno(String nome, String email, int cpf, int telefone, Date dataNascimento, Curso curso, EnumStatusAluno statusAluno,
+                 String inicioCursoAluno, String terminoCursoAluno){
+        
+        super(nome, email, cpf, telefone, dataNascimento);
+        this.curso = curso;
+        this.statusAluno = statusAluno;
+        this.inicioCursoAluno = inicioCursoAluno;
+        this.terminoCursoAluno = terminoCursoAluno;
+        
+    }
+
+    public Curso getCurso() {
+        return curso;
+    }
+
+    public void setCurso(Curso curso) {
+        this.curso = curso;
+    }
+
+    public String getInicioCursoAluno() {
+        return inicioCursoAluno;
+    }
+
+    public void setInicioCursoAluno(String inicioCursoAluno) {
+        this.inicioCursoAluno = inicioCursoAluno;
+    }
+
+    public EnumStatusAluno getStatusAluno() {
+        return statusAluno;
+    }
+
+    public void setStatusAluno(EnumStatusAluno statusAluno) {
+        this.statusAluno = statusAluno;
+    }
+
+    public String getTerminoCursoAluno() {
+        return terminoCursoAluno;
+    }
+
+    public void setTerminoCursoAluno(String terminoCursoAluno) {
+        this.terminoCursoAluno = terminoCursoAluno;
+    }
+    
+    
     
 }
