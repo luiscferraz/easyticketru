2eac51774ca201351b8ac0e9b30ce9ab80227619
nandacms@gmail.com <nandacms@gmail.com@5cbca821-5f6d-a01b-806f-ce45c106f15d>
2013-07-22 18:06:04 +0000

    Criação dos parâmetros da classe Pagamento e métodos de encapsulamento da mesma.

--- EasyTicket/src/negocio/Pagamento.java
+++ EasyTicket/src/negocio/Pagamento.java
@@ -4,10 +4,69 @@
  */
 package negocio;
 
+import java.util.Date;
+
 /**
  *
  * @author Nanda
  */
 public class Pagamento {
+    private int idPagamento;
+    private Date dataPagamento;
+    private float valor;
+    
+    public Pagamento(int idPagamento, Date dataPagamento,float valor){
+        this.idPagamento = idPagamento;
+        this.dataPagamento = dataPagamento;
+        this.valor = valor;
+    }
+
+    /**
+     * @return the idPagamento
+     */
+    public int getIdPagamento() {
+        return idPagamento;
+    }
+
+    /**
+     * @param idPagamento the idPagamento to set
+     */
+    public void setIdPagamento(int idPagamento) {
+        this.idPagamento = idPagamento;
+    }
+
+    /**
+     * @return the dataPagamento
+     */
+    public Date getDataPagamento() {
+        return dataPagamento;
+    }
+
+    /**
+     * @param dataPagamento the dataPagamento to set
+     */
+    public void setDataPagamento(Date dataPagamento) {
+        this.dataPagamento = dataPagamento;
+    }
+
+    /**
+     * @return the valor
+     */
+    public float getValor() {
+        return valor;
+    }
+
+    /**
+     * @param valor the valor to set
+     */
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+    
+    
+    
+    
     
 }
+
+
