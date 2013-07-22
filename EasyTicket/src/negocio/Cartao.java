3dff7b6050dfb193ae8a5be69c76c44e9a65e433
nandacms@gmail.com <nandacms@gmail.com@5cbca821-5f6d-a01b-806f-ce45c106f15d>
2013-07-22 17:54:00 +0000

    Definição dos parâmetros da classe Cartao e métodos de encapsulamento da mesma.

--- EasyTicket/src/negocio/Cartao.java
+++ EasyTicket/src/negocio/Cartao.java
@@ -9,5 +9,73 @@ package negocio;
  * @author Nanda
  */
 public class Cartao {
+    private int idCartao;
+    private int numCartao;
+    private float saldo;
+    private EnumStatusCartao status;
+    
+    public Cartao(int idCartao,int numCartao){
+        this.idCartao = idCartao;
+        this.numCartao = numCartao;
+        this.status = EnumStatusCartao.ATIVO;
+        this.saldo=0;       
+    }
+
+    /**
+     * @return the idCartao
+     */
+    public int getIdCartao() {
+        return idCartao;
+    }
+
+    /**
+     * @param idCartao the idCartao to set
+     */
+    public void setIdCartao(int idCartao) {
+        this.idCartao = idCartao;
+    }
+
+    /**
+     * @return the numCartao
+     */
+    public int getNumCartao() {
+        return numCartao;
+    }
+
+    /**
+     * @param numCartao the numCartao to set
+     */
+    public void setNumCartao(int numCartao) {
+        this.numCartao = numCartao;
+    }
+
+    /**
+     * @return the saldo
+     */
+    public float getSaldo() {
+        return saldo;
+    }
+
+    /**
+     * @param saldo the saldo to set
+     */
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+
+    /**
+     * @return the status
+     */
+    public EnumStatusCartao getStatus() {
+        return status;
+    }
+
+    /**
+     * @param status the status to set
+     */
+    public void setStatus(EnumStatusCartao status) {
+        this.status = status;
+    }
+    
     
 }
