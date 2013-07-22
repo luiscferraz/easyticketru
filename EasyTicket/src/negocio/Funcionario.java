a15adfe9dae10a673cb41ac7145c0213c1ef45d2
marcela.domingues@gmail.com <marcela.domingues@gmail.com@5cbca821-5f6d-a01b-806f-ce45c106f15d>
2013-07-22 20:15:50 +0000

    Criação das classes Aluno, Funcionario e Pessoa.

--- EasyTicket/src/negocio/Funcionario.java
+++ EasyTicket/src/negocio/Funcionario.java
@@ -4,10 +4,40 @@
  */
 package negocio;
 
+import java.util.Date;
+
 /**
  *
  * @author Nanda
  */
-public class Funcionario {
+public class Funcionario extends Pessoa {
+    
+    private String cargo;
+    private EnumStatusFuncionario statusFuncionario;
+    
+    public Funcionario(String nome, String email, int cpf, int telefone, Date dataNascimento, String cargo, 
+                        EnumStatusFuncionario statusFuncionario){
+        
+        super(nome, email, cpf, telefone, dataNascimento);
+        this.cargo = cargo;
+        this.statusFuncionario = statusFuncionario;
+       
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public EnumStatusFuncionario getStatusFuncionario() {
+        return statusFuncionario;
+    }
+
+    public void setStatusFuncionario(EnumStatusFuncionario statusFuncionario) {
+        this.statusFuncionario = statusFuncionario;
+    }
     
 }
--- EasyTicket/src/negocio/Pessoa.java
+++ EasyTicket/src/negocio/Pessoa.java
@@ -4,11 +4,66 @@
  */
 package negocio;
 
+import java.util.Date;
 /**
  *
  * @author Nanda
  */
 public class Pessoa {
+    
+    private String nome;
+    private String email;
+    private int cpf;
+    private int telefone;
+    private Date dataNascimento;
+    
+    public Pessoa(String nome, String email, int cpf, int telefone, Date dataNascimento){
+        this.nome = nome;
+        this.email = email;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
     
     
 }
