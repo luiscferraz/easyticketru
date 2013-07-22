c351774874d484600ad0aaf0a3c79d35950a89a8
nandacms@gmail.com <nandacms@gmail.com@5cbca821-5f6d-a01b-806f-ce45c106f15d>
2013-07-22 18:16:44 +0000

    Definição dos parâmetros e criação dos métodos de encapsulamento da classe Recarga.

--- trunk/EasyTicket/src/negocio/Recarga.java
+++ trunk/EasyTicket/src/negocio/Recarga.java
@@ -4,10 +4,66 @@
  */
 package negocio;
 
+import java.util.Date;
+
 /**
  *
  * @author Nanda
  */
 public class Recarga {
+    private int idRecarga;
+    private float valor;
+    private Date dataRecarga;
+    
+    public Recarga(int idRecarga,float valor, Date dataRecarga){
+        this.idRecarga = idRecarga;
+        this.valor = valor;
+        this.dataRecarga = dataRecarga;
+    }
+
+    /**
+     * @return the idRecarga
+     */
+    public int getIdRecarga() {
+        return idRecarga;
+    }
+
+    /**
+     * @param idRecarga the idRecarga to set
+     */
+    public void setIdRecarga(int idRecarga) {
+        this.idRecarga = idRecarga;
+    }
+
+    /**
+     * @return the valor
+     */
+    public float getValor() {
+        return valor;
+    }
+
+    /**
+     * @param valor the valor to set
+     */
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+
+    /**
+     * @return the dataRecarga
+     */
+    public Date getDataRecarga() {
+        return dataRecarga;
+    }
+
+    /**
+     * @param dataRecarga the dataRecarga to set
+     */
+    public void setDataRecarga(Date dataRecarga) {
+        this.dataRecarga = dataRecarga;
+    }
+    
+    
+    
     
 }
