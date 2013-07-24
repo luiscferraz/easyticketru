1675fd504ace4415b49784609aaf54bc8a25c692
nandacms@gmail.com <nandacms@gmail.com@5cbca821-5f6d-a01b-806f-ce45c106f15d>
2013-07-24 00:09:38 +0000

    Criação dos métodos da interface IRepositorioRecargas.

--- trunk/EasyTicket/src/interfaces/IRepositorioRecargas.java
+++ trunk/EasyTicket/src/interfaces/IRepositorioRecargas.java
@@ -4,10 +4,19 @@
  */
 package interfaces;
 
+import java.util.ArrayList;
+import java.util.Date;
+import negocio.Recarga;
+
 /**
  *
  * @author Nanda
  */
 public interface IRepositorioRecargas {
-    
+    public void inserir(Recarga recarga);
+    public boolean existe(Recarga recarga);
+    public void atualizar(Recarga recarga);
+    public Recarga buscarPorId(int idRecarga);
+    public ArrayList<Recarga> buscarPorCartao(int idCartao);
+    public ArrayList<Recarga> buscarPorData(Date data);
 }
