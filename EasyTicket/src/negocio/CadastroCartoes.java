a715f006faac98ed2f2631445d7ac42f90b93f6d
nandacms@gmail.com <nandacms@gmail.com@5cbca821-5f6d-a01b-806f-ce45c106f15d>
2013-07-29 17:25:49 +0000

    Criação dos métodos atualizar e cadastrar na classe CadastroCartoes.

--- EasyTicket/src/negocio/CadastroCartoes.java
+++ EasyTicket/src/negocio/CadastroCartoes.java
@@ -9,5 +9,24 @@ package negocio;
  * @author Nanda
  */
 public class CadastroCartoes {
+  private RepositorioCartoes cartoes;
+  
+  public CadastroCartoes(RepositorioCartoes c){
+    this.cartoes = c;  
+  }
+  
+  public void atualizar(Cartao c){
+    cartoes.atualizar(c);  
+  }
+  
+  public void cadastrar(Cartao c){
+    //definir as condições para ver se um aluno(verificar por CPF) já tem um cartão cadstrado.
+  }
+  
+  //definir os outros métodos para ativa, desetiva e bloquear um cartão. É preciso também a emissão de saldo do mesmo.
+  
+  //fazer esses outros métodos após a definição da classe de repositório.
+  
+  
     
 }
