c4eca1e81428e1be892e201262337686a1b86aef
nandacms@gmail.com <nandacms@gmail.com@5cbca821-5f6d-a01b-806f-ce45c106f15d>
2013-07-29 18:06:46 +0000

    Modificação para a inclusão dos métodos na classe RepositorioCartoes.

--- EasyTicket/src/dados/RepositorioCartoes.java
+++ EasyTicket/src/dados/RepositorioCartoes.java
@@ -9,5 +9,24 @@ package dados;
  * @author Nanda
  */
 public class RepositorioCartoes {
+    //métodos apenas com assinatura, ainda falta definir a interação com o banco de dados.
+    public void inserir(Cartao cartao){
+    }
+    
+    public boolean existe(Cartao cartao){
+      return boolean;
+    }
+    
+    public void atualizar(Cartao cartao){
+      
+    }
+      
+    public Cartao procurarPorNumero(int numeroCartao){
+      return cartao;
+    }
+    
+    public Cartao procurarPorCpf(int cpf){
+      return cartao;
+    } 
     
 }
