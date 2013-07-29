e97850b2ed6c3244c00c259f84a958509e4ee9e7
nandacms@gmail.com <nandacms@gmail.com@5cbca821-5f6d-a01b-806f-ce45c106f15d>
2013-07-29 18:19:22 +0000

    Modificações na classe de fachada para a inclusão dos métodos obterInstancia e initCadastros.

--- trunk/EasyTicket/src/negocio/Fachada.java
+++ trunk/EasyTicket/src/negocio/Fachada.java
@@ -9,13 +9,39 @@ package negocio;
  * @author Nanda
  */
 public class Fachada {
-  RepositorioCartoes repositorioCartoes = new RepositorioCartoes();
-  cadastroCartoes = new CadastroCartoes(repCartoes);
+
+  private static Fachada instancia;
+  private CadastroCartoes cartoes;
+  private CadastroRefeicoes refeicoes;
   
-  RepositorioRefeicoes repositorioRefeicoes = new RepositorioRefeicoes();
-  cadastroRefeicoes = new CadastroRefeicoes(repositorioRefeicoes);
   
-  RepositorioCursos repositorioCursos = new RepositorioCursos();
-  cadastroCursos = new CadastroCursos(repositorioCursos);
+  //verifica se já existe um,a instancia da fachada em funcionamento
+  private static Fachada obterInstancia(){
+    if(instancia == null){
+      instancia = new Fachada();
+    }
+    return instancia;
+  }
+  
+  //contrutor da fachada que determina que a mesma é iniciada já com todas as funções de cadastro
+  private Fachada(){
+    initCadastros();
+  }
+    
+  //método que irá instanciar todas as classes de cadastro e de repositório para que será utilizado pela fachada  
+  private void initCadastros(){
+    
+    RepositorioCartoes repositorioCartoes = new RepositorioCartoes();
+    cadastroCartoes = new CadastroCartoes(repCartoes);
+  
+    RepositorioRefeicoes repositorioRefeicoes = new RepositorioRefeicoes();
+    cadastroRefeicoes = new CadastroRefeicoes(repositorioRefeicoes);
+  
+    RepositorioCursos repositorioCursos = new RepositorioCursos();
+    cadastroCursos = new CadastroCursos(repositorioCursos);
+    
+  }
+  
+
     
 }
