2c5e00c5c94d83664f0a69c081ecfd6b3fc0aa8e
nandacms@gmail.com <nandacms@gmail.com@5cbca821-5f6d-a01b-806f-ce45c106f15d>
2013-07-31 00:39:52 +0000

    Correção na classe RepositorioFuncionarios e no Diagrama de Classe.

--- EasyTicket/src/dados/RepositorioFuncionarios.java
+++ EasyTicket/src/dados/RepositorioFuncionarios.java
@@ -4,10 +4,39 @@
  */
 package dados;
 
+import interfaces.IRepositorioFuncionarios;
+import java.util.ArrayList;
+import negocio.Funcionario;
+
 /**
  *
  * @author Nanda
  */
-public class RepositorioFuncionarios {
+public class RepositorioFuncionarios implements IRepositorioFuncionarios{
+
+    @Override
+    public void inserir(Funcionario funcionario) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean existe(int cpf) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void atualizar(Funcionario funcionario) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Funcionario procurarPorCpf(int cpf) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<Funcionario> buscarPorCargo(String cargoFuncionario) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
