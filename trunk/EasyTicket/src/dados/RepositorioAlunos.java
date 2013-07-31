8b440b1bf8de672939495ea3ee37f0a8533fa03b
nandacms@gmail.com <nandacms@gmail.com@5cbca821-5f6d-a01b-806f-ce45c106f15d>
2013-07-31 00:34:01 +0000

    Atualização de classes e respectivas representações no Diagrama de Classe - versão 1.asta

--- trunk/EasyTicket/src/dados/RepositorioAlunos.java
+++ trunk/EasyTicket/src/dados/RepositorioAlunos.java
@@ -4,10 +4,39 @@
  */
 package dados;
 
+import interfaces.IRepositorioAlunos;
+import java.util.ArrayList;
+import negocio.Aluno;
+
 /**
  *
  * @author Nanda
  */
-public class RepositorioAlunos {
+public class RepositorioAlunos implements IRepositorioAlunos {
+
+    @Override
+    public void inserir(Aluno aluno) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean existe(int cpf) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void atualizar(Aluno aluno) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Aluno procurarPorCpf(int cpf) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<Aluno> buscarPorCurso(int idCurso) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
