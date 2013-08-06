a1b39bbe35e5deef65ae64fb6bc53e9dcc0fb9fc
nandacms@gmail.com <nandacms@gmail.com@5cbca821-5f6d-a01b-806f-ce45c106f15d>
2013-08-06 10:22:58 +0000

    Implementação dos métodos da classe GenericDAO.

--- EasyTicket/src/dados/GenericDAO.java
+++ EasyTicket/src/dados/GenericDAO.java
@@ -6,6 +6,10 @@ package dados;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -24,4 +28,48 @@ public class GenericDAO {
             return null;
         }
     }
+    
+    public Statement getStatement() throws SQLException {
+        return getConnection().createStatement();
+    }
+
+    public PreparedStatement getStatement(String st) throws SQLException {
+        return getConnection().prepareStatement(st);
+    }
+
+    public ResultSet executeQuery(String query,Object... params) throws SQLException {
+        PreparedStatement ps = getStatement(query);
+        for (int i = 0; i < params.length; i++) {
+            ps.setObject(i+1, params[i]);
+        }
+        return ps.executeQuery();
+    }
+
+    public int executeCommand(String query,Object... params) throws SQLException {
+        int result;
+        try (PreparedStatement ps = getStatement(query)) {
+            for (int i = 0; i < params.length; i++) {
+                try {
+                ps.setObject(i+1, params[i]);
+                } catch (Exception e) {
+                    System.out.println("Error to try "+i+" with value "+params[i]);
+                }
+            }
+            result = ps.executeUpdate();
+        }
+        return result;
+    }
+
+    public Integer getNextId(String tableName) throws SQLException {
+        ResultSet rs = executeQuery("select MAX(ID) from APP."+tableName);
+        rs.next();
+        Object result = rs.getObject(1);
+        if (result == null) {
+            rs.close();
+            return 1;
+        } else {
+            return ((Integer)result)+1;
+        }
+    }
+
 }
