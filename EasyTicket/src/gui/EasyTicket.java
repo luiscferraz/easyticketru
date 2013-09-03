c317b2928da715e32836423c53f4f200b0114583
nandacms@gmail.com <nandacms@gmail.com@5cbca821-5f6d-a01b-806f-ce45c106f15d>
2013-09-03 18:38:04 +0000

    Criação de classe principal.

---
+++ EasyTicket/src/gui/EasyTicket.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+/**
+ *
+ * @author Nanda
+ */
+public class EasyTicket {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        TelaPrincipalFuncionario tela = new TelaPrincipalFuncionario();
+        tela.setVisible(true);
+        
+    }
+}
