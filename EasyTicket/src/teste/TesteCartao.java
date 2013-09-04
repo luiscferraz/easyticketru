a979f88d1b7c1c842e38cbb2c20593f795723776
nandacms@gmail.com <nandacms@gmail.com@5cbca821-5f6d-a01b-806f-ce45c106f15d>
2013-09-04 08:05:31 +0000

    Atualização

--- EasyTicket/src/teste/TesteCartao.java
+++ EasyTicket/src/teste/TesteCartao.java
@@ -15,15 +15,7 @@ public class TesteCartao {
  
      public static void main(String[] args) {
         // TODO code application logic here
-        Cartao cartao = new Cartao();
-        cartao.setNumCartao(21340);
-        cartao.setStatus("ATIVO");
-        cartao.setSaldo(0);
-        cartao.setIdAlunoCartao(1);
         
-        RepositorioCartoes repositorio = new RepositorioCartoes();
-        
-        repositorio.inserir(cartao);
         
         
  
