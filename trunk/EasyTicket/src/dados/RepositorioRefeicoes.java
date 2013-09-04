0db3ce8ed9ac4eb3d2bf0b85d4c144e1c79ae219
allandoamaral@gmail.com <allandoamaral@gmail.com@5cbca821-5f6d-a01b-806f-ce45c106f15d>
2013-09-04 01:05:30 +0000

    Inserir Refeicoes

--- trunk/EasyTicket/src/dados/RepositorioRefeicoes.java
+++ trunk/EasyTicket/src/dados/RepositorioRefeicoes.java
@@ -5,6 +5,11 @@
 package dados;
 
 import interfaces.IRepositorioRefeicoes;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import negocio.Refeicao;
 
 /**
@@ -12,10 +17,37 @@ import negocio.Refeicao;
  * @author Nanda
  */
 public class RepositorioRefeicoes implements IRepositorioRefeicoes {
+    
+    private Connection conexao;
+    
+    public RepositorioRefeicoes(){
+        try {
+            this.conexao = GenericDAO.getConnection();
+        } catch (Exception ex) {
+            Logger.getLogger(RepositorioRefeicoes.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 
     @Override
     public void inserir(Refeicao refeicao) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String query = "INSERT INTO EASYTICKET.REFEICOES"+
+                                          " (DESCRICAOREFEICAO,VALORREFEICAO)"+
+                        "VALUES (?,?)" ;
+        
+       
+            try {
+                PreparedStatement stmt = this.conexao.prepareStatement(query);
+
+                stmt.setString(1, refeicao.getDescricao());
+                stmt.setString(2, refeicao.getValor());
+
+                stmt.execute();
+
+                //conexao.close();
+                System.out.println("Refeição inserida com sucesso.");
+            } catch (SQLException ex) {
+                System.out.println("incluirRefeição(): "+ex.toString());
+            } 
     }
 
     @Override
